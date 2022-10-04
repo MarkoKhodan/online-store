031ccde751f225491cedc811e58eadb315461799
Marko <marko@MacBook-Pro-Marko.local>
2022-10-04 20:34:12 +0300

    items list passed into index view context, home page contains test passed

--- store/tests.py
+++ store/tests.py
@@ -4,7 +4,9 @@ client = Client()
 
 
 class TestUrl(TestCase):
+    fixtures = ["item.json"]
+
     def test_home_page(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "<title>IPHONE 14</title>")
+        self.assertContains(response, "<h2> IPHONE 14 </h2>")
