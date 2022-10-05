edd58c518887de1bc66ba0a06fb2fe0960d242c2
Marko <marko@MacBook-Pro-Marko.local>
2022-10-05 23:38:37 +0300

    sidebar and login-logout added

--- store/tests.py
+++ store/tests.py
@@ -15,6 +15,7 @@ class TestUrl(TestCase):
         response = self.client.get("/sale/1/")
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "IPHONE 14")
+
         response = self.client.get("/sale/2/")
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "MacBook Pro")
